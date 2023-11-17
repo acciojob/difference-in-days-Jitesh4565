7978e25a955091c7def0a82d0de01c2b66b7f026
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-17 10:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const d1=new Date(date1);
+	const d2=new Date(date2);
+
+	const timeDiff=d2.getTime() - d1.getTime();
+
+	const days=Math.floor(timeDiff/(1000*60*60*24));
+
+	return days;
 };
 
 // Do not change the code below.
